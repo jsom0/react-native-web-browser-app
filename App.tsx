e8710ed678a1c39a4b3967b2c406395aba9630fa
parthkanani93 <parthkanani8490@gmail.com>
2021-10-04 23:17:08 +0530

    added navigation library

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {View, Text} from 'react-native';
 import {store} from './src/store/store';
-import {BrowserViewControllerConnected} from './src/browser/BrowserViewController';
+import {BrowserViewControllerConnected} from './src/browser/BrowserViewController.tsx';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 interface Props {}
 
 interface State {}
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
 
 class AppContainer extends React.Component<Props, State> {
   render() {
@@ -15,7 +27,9 @@ class AppContainer extends React.Component<Props, State> {
     return (
       <SafeAreaProvider>
         <Provider store={store}>
-          <BrowserViewControllerConnected />
+          <NavigationContainer>
+            <BrowserViewControllerConnected />
+          </NavigationContainer>
         </Provider>
       </SafeAreaProvider>
     );
--- android/app/src/main/java/com/reactnativewebbrowserapp/MainActivity.java
+++ android/app/src/main/java/com/reactnativewebbrowserapp/MainActivity.java
@@ -1,4 +1,5 @@
 package com.reactnativewebbrowserapp;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 
@@ -12,4 +13,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "reactNativeWebBrowserApp";
   }
+  @Override
+protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
+
+  
 }
